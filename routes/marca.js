3d890650ab6626f409a681302976678dd260fc7a
FzrFernando <fernandozamorarico@gmail.com>
2023-06-06 10:51:49 +0200

    Hacer check

--- routes/marca.js
+++ routes/marca.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const { check } = require('express-validator')
 
 const {getMarcas, getMarca, addMarca, updateMarca, deleteMarca} = require('../controllers/marca')
 
 router.get('/',getMarcas)
+
 router.get('/:id',getMarca)
+
 router.post('/',addMarca)
-router.put('/:id',updateMarca)
-router.delete('/:id', deleteMarca)
+
+router.put('/:id', [
+    check('id','No es un id correcto').isMongoId(),
+    check('nombre','El nombre es obligatorio').not().isEmpty()
+], updateMarca)
+
+router.delete('/:id',[
+    check('id','No es un id correcto').isMongoId()
+], deleteMarca)
 
 module.exports = router
